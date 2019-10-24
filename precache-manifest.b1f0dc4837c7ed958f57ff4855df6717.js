bceaa49e0a44bb5ab9e8a47cb080648e11e41a22
Drew11 <drew.naumenko@gmail.com>
2019-10-24 19:34:06 +0300

    Updates

---
+++ precache-manifest.b1f0dc4837c7ed958f57ff4855df6717.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae888b070657659f3ee91ba55509a641",
+    "url": "/test-klickly/index.html"
+  },
+  {
+    "revision": "ef3659d2edfdfe7c339f",
+    "url": "/test-klickly/static/css/main.df6e0008.chunk.css"
+  },
+  {
+    "revision": "105d0ca744311eebde6d",
+    "url": "/test-klickly/static/js/2.f690ff1f.chunk.js"
+  },
+  {
+    "revision": "ef3659d2edfdfe7c339f",
+    "url": "/test-klickly/static/js/main.84a06fdf.chunk.js"
+  },
+  {
+    "revision": "22259c89982ca3242361",
+    "url": "/test-klickly/static/js/runtime-main.d4f1a2de.js"
+  },
+  {
+    "revision": "2d2ae2556b24a45ff8d5ed86b07b5783",
+    "url": "/test-klickly/static/media/ProximaNova-Regular.2d2ae255.woff"
+  },
+  {
+    "revision": "426c254d5cf61d1feaabf79607946715",
+    "url": "/test-klickly/static/media/card-logos.426c254d.svg"
+  },
+  {
+    "revision": "67ccaa103ad8ac213e4a9768e882c585",
+    "url": "/test-klickly/static/media/header-logo.67ccaa10.svg"
+  },
+  {
+    "revision": "05d47c5cb906415732dfa1bb11b1d69d",
+    "url": "/test-klickly/static/media/plante.05d47c5c.jpg"
+  },
+  {
+    "revision": "1760472e1eea5f9151ec4ac2bca6d395",
+    "url": "/test-klickly/static/media/rectangle2x.1760472e.png"
+  }
+]);
